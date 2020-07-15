d71f5480352e822d9c44ef9582eb56825b3cc661
FYLSunghwan <sunghwan519@hotmail.com>
2020-07-15 23:11:23 +0900

    add: Add audiveris route

--- forte_flask/views/routes.py
+++ forte_flask/views/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+import subprocess
+import os
+from flask import Blueprint, request, jsonify
 
 routes_blueprint = Blueprint('routes', __name__)
 
 @routes_blueprint.route("/")
 def index():
     return "Test Routing"
+
+@routes_blueprint.route("/audiveris")
+def audiveris():
+    if request.method == 'POST':
+        #file = request.files['file']
+        #file.save('test')
+        #subprocess.call(["sudo","docker","run","toprock/audiveris"])
+        return 'success'
---
+++ test.py
@@ -0,0 +1,5 @@
+import requests
+url='http://0.0.0.0:5000/audiveris'
+values={'TEST':'test'}
+files={'file':open('test.py','rb')}
+r = requests.post(url,files=files,data=values)
